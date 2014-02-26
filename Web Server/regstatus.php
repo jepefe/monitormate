16e5a90633c1d9d19ecd7e77811785586ab1a4aa
Timothy Martin <instanttim@server.fake>
2014-02-25 23:09:17 -0800

    code tidying for readability.

--- Web Server/regstatus.php
+++ Web Server/regstatus.php
@@ -55,8 +55,8 @@ if(isset($_POST)){
 						register_flexnet($i, $date_time);
 						break;
 					case '3': //FM/MX device id
-						$fmmx_total["total_daily_kwh"] = $fmmx_total["total_daily_kwh"] + $i["daily_kwh"];
-						$fmmx_total["total_daily_ah"] = $fmmx_total["total_daily_ah"] + $i["daily_ah"];
+						$fmmx_total["total_daily_kwh"]	= $fmmx_total["total_daily_kwh"] + $i["daily_kwh"];
+						$fmmx_total["total_daily_ah"]	= $fmmx_total["total_daily_ah"] + $i["daily_ah"];
 						register_fmmx($i, $date_time);
 						break;
 					case '2': //FX device id
@@ -69,14 +69,14 @@ if(isset($_POST)){
 			}
 			
 			if ($flexnet_available != 0) {
-				$summary["kwh_in"] =  $flexnet_available["today_net_input_kwh"];
-				$summary["kwh_out"] =  $flexnet_available["today_net_output_kwh"];
-				$summary["ah_in"] =  $flexnet_available["today_net_input_ah"]; 
-				$summary["ah_out"] =  $flexnet_available["today_net_output_ah"];
-				$summary["min_soc"] = $flexnet_available['today_min_soc'];
+				$summary["kwh_in"]	= $flexnet_available["today_net_input_kwh"];
+				$summary["kwh_out"] = $flexnet_available["today_net_output_kwh"];
+				$summary["ah_in"]	= $flexnet_available["today_net_input_ah"]; 
+				$summary["ah_out"]	= $flexnet_available["today_net_output_ah"];
+				$summary["min_soc"]	= $flexnet_available['today_min_soc'];
 			} else {
-				$summary["kwh_in"] =  $fmmx_total["total_daily_kwh"];
-				$summary["ah_in"] =  $fmmx_total["total_daily_ah"]; 
+				$summary["kwh_in"]	=  $fmmx_total["total_daily_kwh"];
+				$summary["ah_in"]	=  $fmmx_total["total_daily_ah"]; 
 			}
 		
 			register_summary($summary);
@@ -95,103 +95,101 @@ if(isset($_POST)){
 
 function register_flexnet($device_array,$date_time) {
 
-	 $query = "INSERT INTO monitormate3_flexnet
-	 	(
-		 	date,
-		 	address,
-		 	device_id,
-		 	shunt_a_amps,
-		 	shunt_b_amps,
-		 	shunt_c_amps,
-		 	accumulated_ah_shunt_a,
-			accumulated_kwh_shunt_a,
-			accumulated_ah_shunt_b,
-			accumulated_kwh_shunt_b,
-			accumulated_ah_shunt_c,
-			accumulated_kwh_shunt_c,
-			days_since_full,
-			today_min_soc,
-			today_net_input_ah,
-			today_net_output_ah,
-			today_net_input_kwh,
-			today_net_output_kwh,
-			charge_factor_corrected_net_batt_ah,
-			charge_factor_corrected_net_batt_kwh,
-			charge_params_met,
-			relay_mode,
-			relay_status,
-			battery_volt,
-			soc,
-			shunt_enabled_a,
-			shunt_enabled_b,
-			shunt_enabled_c,
-			battery_temp
-		) VALUES (
-			'".$date_time."',
-			'".$device_array['address']."',
-			'".$device_array['device_id']."',
-			'".$device_array['shunt_a_amps']."',
-			'".$device_array['shunt_b_amps']."',
-			'".$device_array['shunt_c_amps']."',
-			'".$device_array['accumulated_ah_shunt_a']."',
-			'".$device_array['accumulated_kwh_shunt_a']."',
-			'".$device_array['accumulated_ah_shunt_b']."',
-			'".$device_array['accumulated_kwh_shunt_b']."',
-			'".$device_array['accumulated_ah_shunt_c']."',
-			'".$device_array['accumulated_kwh_shunt_c']."',
-			'".$device_array['days_since_full']."',
-			'".$device_array['today_min_soc']."',
-			'".$device_array['today_net_input_ah']."',
-			'".$device_array['today_net_output_ah']."',
-			'".$device_array['today_net_input_kwh']."',
-			'".$device_array['today_net_output_kwh']."',
-			'".$device_array['charge_factor_corrected_net_batt_ah']."',
-			'".$device_array['charge_factor_corrected_net_batt_kwh']."',
-			'".$device_array['charge_params_met']."',
-			'".$device_array['relay_mode']."',
-			'".$device_array['relay_status']."',
-			'".$device_array['battery_volt']."',
-			'".$device_array['soc']."',
-			'".$device_array['shunt_enabled_a']."',
-			'".$device_array['shunt_enabled_b']."',
-			'".$device_array['shunt_enabled_c']."',
-			'".$device_array['battery_temp']."'
-		)";
-		
+	 $query =	"INSERT INTO monitormate3_flexnet (
+				 	date,
+				 	address,
+				 	device_id,
+				 	shunt_a_amps,
+				 	shunt_b_amps,
+				 	shunt_c_amps,
+				 	accumulated_ah_shunt_a,
+					accumulated_kwh_shunt_a,
+					accumulated_ah_shunt_b,
+					accumulated_kwh_shunt_b,
+					accumulated_ah_shunt_c,
+					accumulated_kwh_shunt_c,
+					days_since_full,
+					today_min_soc,
+					today_net_input_ah,
+					today_net_output_ah,
+					today_net_input_kwh,
+					today_net_output_kwh,
+					charge_factor_corrected_net_batt_ah,
+					charge_factor_corrected_net_batt_kwh,
+					charge_params_met,
+					relay_mode,
+					relay_status,
+					battery_volt,
+					soc,
+					shunt_enabled_a,
+					shunt_enabled_b,
+					shunt_enabled_c,
+					battery_temp
+				) VALUES (
+					'".$date_time."',
+					'".$device_array['address']."',
+					'".$device_array['device_id']."',
+					'".$device_array['shunt_a_amps']."',
+					'".$device_array['shunt_b_amps']."',
+					'".$device_array['shunt_c_amps']."',
+					'".$device_array['accumulated_ah_shunt_a']."',
+					'".$device_array['accumulated_kwh_shunt_a']."',
+					'".$device_array['accumulated_ah_shunt_b']."',
+					'".$device_array['accumulated_kwh_shunt_b']."',
+					'".$device_array['accumulated_ah_shunt_c']."',
+					'".$device_array['accumulated_kwh_shunt_c']."',
+					'".$device_array['days_since_full']."',
+					'".$device_array['today_min_soc']."',
+					'".$device_array['today_net_input_ah']."',
+					'".$device_array['today_net_output_ah']."',
+					'".$device_array['today_net_input_kwh']."',
+					'".$device_array['today_net_output_kwh']."',
+					'".$device_array['charge_factor_corrected_net_batt_ah']."',
+					'".$device_array['charge_factor_corrected_net_batt_kwh']."',
+					'".$device_array['charge_params_met']."',
+					'".$device_array['relay_mode']."',
+					'".$device_array['relay_status']."',
+					'".$device_array['battery_volt']."',
+					'".$device_array['soc']."',
+					'".$device_array['shunt_enabled_a']."',
+					'".$device_array['shunt_enabled_b']."',
+					'".$device_array['shunt_enabled_c']."',
+					'".$device_array['battery_temp']."'
+				)";
+				
 	$connection = db_connection();
 	mysql_query($query,$connection);
 }
 
 
 function register_fmmx($device_array,$date_time){
-	$query = "INSERT INTO monitormate3_fmmx
-		(
-			date,
-			address,
-			device_id,
-			charge_current,
-			pv_current,
-			pv_voltage,
-			daily_kwh,
-			aux_mode,
-			error_modes,
-			charge_mode,
-			battery_volts,
-			daily_ah
-		) VALUES (
-			'".$date_time."',
-			'".$device_array['address']."',
-			'".$device_array['device_id']."',
-			'".$device_array['charge_current']."',
-			'".$device_array['pv_current']."',
-			'".$device_array['pv_voltage']."',
-			'".$device_array['daily_kwh']."',
-			'".$device_array['aux_mode']."',
-			'".implode(",", $device_array['error_modes'])."',
-			'".$device_array['charge_mode']."',
-			'".$device_array['battery_volts']."',
-			'".$device_array['daily_ah']."'
-		)";
+	$query =	"INSERT INTO monitormate3_fmmx (
+					date,
+					address,
+					device_id,
+					charge_current,
+					pv_current,
+					pv_voltage,
+					daily_kwh,
+					aux_mode,
+					error_modes,
+					charge_mode,
+					battery_volts,
+					daily_ah
+				) VALUES (
+					'".$date_time."',
+					'".$device_array['address']."',
+					'".$device_array['device_id']."',
+					'".$device_array['charge_current']."',
+					'".$device_array['pv_current']."',
+					'".$device_array['pv_voltage']."',
+					'".$device_array['daily_kwh']."',
+					'".$device_array['aux_mode']."',
+					'".implode(",", $device_array['error_modes'])."',
+					'".$device_array['charge_mode']."',
+					'".$device_array['battery_volts']."',
+					'".$device_array['daily_ah']."'
+				)";
 	
 	$connection = db_connection();
 	mysql_query($query,$connection);
@@ -199,39 +197,38 @@ function register_fmmx($device_array,$date_time){
 
 
 function register_fxinv($device_array,$date_time){
-	$query = "INSERT INTO monitormate3_fxinv
-		(
-			date,
-			address,device_id,
-			inverter_current,
-			charge_current,
-			buy_current,
-			ac_input_voltage,
-			ac_output_voltage,
-			sell_current,
-			operational_mode,
-			error_modes,
-			ac_mode,
-			battery_volt,
-			misc,
-			warning_modes
-		) VALUES (
-			'".$date_time."',
-			'".$device_array['address']."',
-			'".$device_array['device_id']."',
-			'".$device_array['inverter_current']."',
-			'".$device_array['charge_current']."',
-			'".$device_array['buy_current']."',
-			'".$device_array['ac_input_voltage']."',
-			'".$device_array['ac_output_voltage']."',
-			'".$device_array['sell_current']."',
-			'".$device_array['operational_mode']."',
-			'".implode(",", $device_array['error_modes'])."',
-			'".$device_array['ac_mode']."',
-			'".$device_array['battery_volt']."',
-			'".$device_array['misc']."',
-			'".implode(",", $device_array['warning_modes'])."'
-		)";
+	$query =	"INSERT INTO monitormate3_fxinv (
+					date,
+					address,device_id,
+					inverter_current,
+					charge_current,
+					buy_current,
+					ac_input_voltage,
+					ac_output_voltage,
+					sell_current,
+					operational_mode,
+					error_modes,
+					ac_mode,
+					battery_volt,
+					misc,
+					warning_modes
+				) VALUES (
+					'".$date_time."',
+					'".$device_array['address']."',
+					'".$device_array['device_id']."',
+					'".$device_array['inverter_current']."',
+					'".$device_array['charge_current']."',
+					'".$device_array['buy_current']."',
+					'".$device_array['ac_input_voltage']."',
+					'".$device_array['ac_output_voltage']."',
+					'".$device_array['sell_current']."',
+					'".$device_array['operational_mode']."',
+					'".implode(",", $device_array['error_modes'])."',
+					'".$device_array['ac_mode']."',
+					'".$device_array['battery_volt']."',
+					'".$device_array['misc']."',
+					'".implode(",", $device_array['warning_modes'])."'
+				)";
 
 	$connection = db_connection();
 	mysql_query($query,$connection);		
@@ -266,25 +263,53 @@ function register_summary($summary) {
 		$summary['max_soc'] = $max_soc[0];
 	}
 	
-	$not_first_record = mysql_query("select date from monitormate3_summary where date(date) ='".$summary['date']."'",$connection);
+	$not_first_record = mysql_query("SELECT date FROM monitormate3_summary WHERE date(date) ='".$summary['date']."'",$connection);
 	$not_first_record = mysql_fetch_row($not_first_record);
 
-	$query = "INSERT INTO monitormate3_summary(date, kwh_in, kwh_out, ah_in, ah_out, max_temp, min_temp, max_soc, min_soc, max_pv_voltage) VALUES ('"
-		.$summary['date']."','".$summary['kwh_in']."','".$summary['kwh_out']."','".$summary['ah_in']."','"
-		.$summary['ah_out']."','".$summary['max_temp']."','".$summary['min_temp']."','".$summary['max_soc']."','"
-		.$summary['min_soc']."','".$summary['max_pv_voltage']."')"; 
+	$query =	"INSERT INTO monitormate3_summary (
+					date,
+					kwh_in,
+					kwh_out,
+					ah_in,
+					ah_out,
+					max_temp,
+					min_temp,
+					max_soc,
+					min_soc,
+					max_pv_voltage
+				) VALUES (
+					'".$summary['date']."',
+					'".$summary['kwh_in']."',
+					'".$summary['kwh_out']."',
+					'".$summary['ah_in']."',
+					'".$summary['ah_out']."',
+					'".$summary['max_temp']."',
+					'".$summary['min_temp']."',
+					'".$summary['max_soc']."',
+					'".$summary['min_soc']."',
+					'".$summary['max_pv_voltage']."'
+				)"; 
 	
-	$update_query = "UPDATE monitormate3_summary SET kwh_in=".$summary['kwh_in'].", kwh_out=".$summary['kwh_out'].", 
-	ah_in=".$summary['ah_in'].", ah_out=".$summary['ah_out'].", max_temp=".$summary['max_temp'].", min_temp=".$summary['min_temp'].
-	", max_soc=".$summary['max_soc'].", min_soc=".$summary['min_soc'].", max_pv_voltage=".$summary['max_pv_voltage']." where date(date)='".$summary['date']."'";
+	$update_query =	"UPDATE monitormate3_summary SET
+						kwh_in=".$summary['kwh_in'].",
+						kwh_out=".$summary['kwh_out'].",
+						ah_in=".$summary['ah_in'].",
+						ah_out=".$summary['ah_out'].",
+						max_temp=".$summary['max_temp'].",
+						min_temp=".$summary['min_temp'].",
+						max_soc=".$summary['max_soc'].",
+						min_soc=".$summary['min_soc'].",
+						max_pv_voltage=".$summary['max_pv_voltage']."
+					WHERE date(date)='".$summary['date']."'";
 	
 	$file22 = "matelog22";
 	file_put_contents($file22, $update_query);
 		
 	if ($not_first_record) {
-		mysql_query($update_query,$connection);
+		// TODO: check to make sure we're not inserting zero'd out data (mate thinks it's the next day)
+		mysql_query($update_query, $connection);
 	} else {
-		mysql_query($query,$connection);
+		mysql_query($query, $connection);
 	}
 }
 
