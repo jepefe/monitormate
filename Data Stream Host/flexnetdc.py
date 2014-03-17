c554182dee9aba0c1a5cda4f89759f21fef65871
Timothy Martin <instanttim@server.fake>
2014-03-16 23:19:52 -0700

    spaces to tabs

--- Data Stream Host/flexnetdc.py
+++ Data Stream Host/flexnetdc.py
@@ -18,151 +18,151 @@
 
 
 class flexnetdc():
-    
-    #Names of all data values in raw_string
-    valuenames = ['address','device_id', 'shunt_a_amps', 'shunt_b_amps','shunt_c_amps','extra_data_id', 'extra_data', 'battery_volts', \
-                  'soc','shunt_enable','status_flags', 'battery_temp']
-    
-    #Names of all formatted data values
-    valuenames_formatted = ['address','device_id', 'shunt_a_amps', 'shunt_b_amps','shunt_c_amps','accumulated_ah_shunt_a','accumulated_kwh_shunt_a',\
-                            'accumulated_ah_shunt_b','accumulated_kwh_shunt_b', 'accumulated_ah_shunt_c', 'accumulated_kwh_shunt_c', \
-                             'days_since_full','today_min_soc','today_net_input_ah','today_net_output_ah','today_net_input_kwh',\
-                             'today_net_output_kwh','charge_factor_corrected_net_batt_ah','charge_factor_corrected_net_batt_kwh',\
-                             'charge_params_met','relay_mode','relay_status', 'battery_volt', 'soc','shunt_enabled_a','shunt_enabled_b',\
-                             'shunt_enabled_c','battery_temp']
-    
-    
-    def __init__(self):
-    
-        self.dev_address = None
-    #Raw datastream
-        self.status_raw = [0]*self.valuenames.__len__()
-    
-    #Formatted datastream
-        self.status_formatted = [0]*self.valuenames_formatted.__len__()
-    #Device name
-        self.name = "FLEXnet DC"
+	
+	#Names of all data values in raw_string
+	valuenames = ['address','device_id', 'shunt_a_amps', 'shunt_b_amps','shunt_c_amps','extra_data_id', 'extra_data', 'battery_volts', \
+				  'soc','shunt_enable','status_flags', 'battery_temp']
+	
+	#Names of all formatted data values
+	valuenames_formatted = ['address','device_id', 'shunt_a_amps', 'shunt_b_amps','shunt_c_amps','accumulated_ah_shunt_a','accumulated_kwh_shunt_a',\
+							'accumulated_ah_shunt_b','accumulated_kwh_shunt_b', 'accumulated_ah_shunt_c', 'accumulated_kwh_shunt_c', \
+							 'days_since_full','today_min_soc','today_net_input_ah','today_net_output_ah','today_net_input_kwh',\
+							 'today_net_output_kwh','charge_factor_corrected_net_batt_ah','charge_factor_corrected_net_batt_kwh',\
+							 'charge_params_met','relay_mode','relay_status', 'battery_volt', 'soc','shunt_enabled_a','shunt_enabled_b',\
+							 'shunt_enabled_c','battery_temp']
+	
+	
+	def __init__(self):
+	
+		self.dev_address = None
+	#Raw datastream
+		self.status_raw = [0]*self.valuenames.__len__()
+	
+	#Formatted datastream
+		self.status_formatted = [0]*self.valuenames_formatted.__len__()
+	#Device name
+		self.name = "FLEXnet DC"
 
-    
+	
 
 #------------------------------#
-# Get and format datastream    #
+# Get and format datastream	   #
 #------------------------------#
-    def set_status(self,datastream):
-        
-        self.status_raw = datastream
+	def set_status(self,datastream):
+		
+		self.status_raw = datastream
 
 		# Port address
-        self.status_formatted[0] = datastream[0]
+		self.status_formatted[0] = datastream[0]
 		# Device ID
-        self.status_formatted[1] = datastream[1]
+		self.status_formatted[1] = datastream[1]
 
 		##
-        ## Get status flags
+		## Get status flags
 		##
 		
-        status_flags = int(datastream[10])
+		status_flags = int(datastream[10])
 
-        # Sign for shunts
-        signA=1
-        signB=1
-        signC=1
+		# Sign for shunts
+		signA=1
+		signB=1
+		signC=1
 
-        if status_flags & 8:
-            signA = -1
-        if status_flags & 16:
-            signB = -1
-        if status_flags & 32:
-            signC = -1
+		if status_flags & 8:
+			signA = -1
+		if status_flags & 16:
+			signB = -1
+		if status_flags & 32:
+			signC = -1
 
-        self.status_formatted[2] = str(float(datastream[2])*0.1*signA)
-        self.status_formatted[3] = str(float(datastream[3])*0.1*signB)
-        self.status_formatted[4] = str(float(datastream[4])*0.1*signC)  
+		self.status_formatted[2] = str(float(datastream[2])*0.1*signA)
+		self.status_formatted[3] = str(float(datastream[3])*0.1*signB)
+		self.status_formatted[4] = str(float(datastream[4])*0.1*signC)	
 
-        #Charge params met
-        if status_flags & 1:
-            self.status_formatted[19] = 'Yes'
-        else:
-            self.status_formatted[19] = 'No'
-        
-        #Relay mode and state 
-        if status_flags & 4:
-            self.status_formatted[20] = 'Manual'
-        else:
-            self.status_formatted[20] = 'Automatic'
-            
-        if status_flags & 2:
-            self.status_formatted[21] = 'Closed'
-        else:
-            self.status_formatted[21] = 'Open'
+		#Charge params met
+		if status_flags & 1:
+			self.status_formatted[19] = 'Yes'
+		else:
+			self.status_formatted[19] = 'No'
+		
+		#Relay mode and state 
+		if status_flags & 4:
+			self.status_formatted[20] = 'Manual'
+		else:
+			self.status_formatted[20] = 'Automatic'
+			
+		if status_flags & 2:
+			self.status_formatted[21] = 'Closed'
+		else:
+			self.status_formatted[21] = 'Open'
 
-        extra_data_id = int(datastream[5])
-        extra_data = int(datastream[6])
+		extra_data_id = int(datastream[5])
+		extra_data = int(datastream[6])
 
 		##
-        ## Extra data 
-        ##
+		## Extra data 
+		##
 
-        #Get extra data sign and if negative (>64) remove bit 7 (subtract 64)
+		#Get extra data sign and if negative (64 or higher) remove bit 7 (subtract 64)
 
-        if extra_data_id > 63:
-            ed_sign = -1
-            extra_data = extra_data - 64
-        else:
-            ed_sign = 1
+		if extra_data_id > 63:
+			ed_sign = -1
+			extra_data = extra_data - 64
+		else:
+			ed_sign = 1
 
-        #Put extra data values in formated status list
-        
-        #One decimal value (days since full charge)
-        if extra_data_id == 6:
-            self.status_formatted[5+6] = extra_data * 0.1 * ed_sign
-            
-        #Values without decimals (Ah values and min SOC.)  
-        nodecimal = [0,2,4,7,8,9,12]
-        if extra_data_id in nodecimal:
-            self.status_formatted[5+extra_data_id] = str(extra_data * ed_sign)
-            
-        #Two decimals values (kWh values.)
-        twodecimals = [1,3,5,10,11,13]
-        if extra_data_id in twodecimals:
-            self.status_formatted[5+extra_data_id] = str(extra_data * 0.01 * ed_sign)
+		#Put extra data values in formated status list
+
+		#One decimal value (days since full charge)
+		if extra_data_id == 6:
+			self.status_formatted[5 + extra_data_id] = str(extra_data * 0.1 * ed_sign)
+			
+		#Values without decimals (Ah values and min SOC.)  
+		nodecimal = [0,2,4,7,8,9,12]
+		if extra_data_id in nodecimal:
+			self.status_formatted[5 + extra_data_id] = str(extra_data * ed_sign)
+			
+		#Two decimals values (kWh values.)
+		twodecimals = [1,3,5,10,11,13]
+		if extra_data_id in twodecimals:
+			self.status_formatted[5 + extra_data_id] = str(extra_data * 0.01 * ed_sign)
 
 		##
 		## End of extra data 
-        ##
+		##
 
-        #Shunt status
-        self.status_formatted[24] = 'On'
-        self.status_formatted[25] = 'On'
-        self.status_formatted[26] = 'On'
+		#Shunt status
+		self.status_formatted[24] = 'On'
+		self.status_formatted[25] = 'On'
+		self.status_formatted[26] = 'On'
 
-        if datastream[9][0] == 1:
-            self.status_formatted[24] = 'Off'
-        if datastream[9][1] == 1:
-            self.status_formatted[25] = 'Off'
-        if datastream[9][2] == 1:
-            self.status_formatted[26] = 'Off'
+		if datastream[9][0] == 1:
+			self.status_formatted[24] = 'Off'
+		if datastream[9][1] == 1:
+			self.status_formatted[25] = 'Off'
+		if datastream[9][2] == 1:
+			self.status_formatted[26] = 'Off'
 
-        #Battery temp
-        self.status_formatted[27] = str(int(datastream[11]) - 10)
+		#Battery temp
+		self.status_formatted[27] = str(int(datastream[11]) - 10)
 
-        #Battery volts
-        self.status_formatted[22] = str(int(datastream[7]) * 0.1)
+		#Battery volts
+		self.status_formatted[22] = str(int(datastream[7]) * 0.1)
 
-        #Soc
-        self.status_formatted[23] = int(datastream[8])
+		#Soc
+		self.status_formatted[23] = int(datastream[8])
 
 #-----------------------------------#
 # Get all device values with labels #
 #-----------------------------------#
-    def get_values_with_names(self):
-     
-        values = {}
-        for idx, i in enumerate(self.valuenames_formatted):       
-            values.update({i:self.status_formatted[idx]})
-        #print values
-        return values
+	def get_values_with_names(self):
+	 
+		values = {}
+		for idx, i in enumerate(self.valuenames_formatted):		  
+			values.update({i:self.status_formatted[idx]})
+		#print values
+		return values
 
-    
+	
 
 
