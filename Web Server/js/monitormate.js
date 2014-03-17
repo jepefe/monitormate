fd5f43eee4daaff3a25315c638107ad412f54d89
Timothy Martin <instanttim@server.fake>
2014-03-17 00:45:54 -0700

    cleaned up battery status fields
    charge factor corrected net battery amp hours is a mouthful.

--- Web Server/js/monitormate.js
+++ Web Server/js/monitormate.js
@@ -525,10 +525,11 @@ function set_status(HTML_id, value) {
 			content =	'<table><caption>' + deviceLabel[parseInt(device.address)] + '<div>Port ' + device.address + '</div></caption>\
 						<tr><td class="label">State of Charge:</td><td>' + device.soc + '%</td></tr>\
 						<tr><td class="label">Days Since Charged:</td><td>' + (Math.round(device.days_since_full * 100) / 100) + ' Days</td></tr>\
+						<tr><td class="label">Charge Required:</td><td>' + device.charge_factor_corrected_net_batt_ah + ' Ah, ' + device.charge_factor_corrected_net_batt_kwh + ' kWh</td></tr>\
 						<tr><td class="label">Within Charge Params:</td><td>' + device.charge_params_met + '</td></tr>\
-						<tr><td class="label">Battery Voltage:</td><td>' + device.battery_volt + ' V</td></tr>\
-						<tr><td class="label">Battery Temperature:</td><td>' + device.battery_temp + ' &deg;C (' + ((device.battery_temp * 1.8) + 32).toFixed(1) + ' &deg;F)</td></tr>\
-						<tr><td class="label">Charge Corrected Net:</td><td>' + device.charge_factor_corrected_net_batt_ah + ' Ah, ' + device.charge_factor_corrected_net_batt_kwh + ' kWh</td></tr>\
+						<th class="subhead">Battery</th>\
+						<tr><td class="label">Voltage:</td><td>' + device.battery_volt + ' V</td></tr>\
+						<tr><td class="label">Temperature:</td><td>' + device.battery_temp + ' &deg;C (' + ((device.battery_temp * 1.8) + 32).toFixed(1) + ' &deg;F)</td></tr>\
 						<th class="subhead">Auxiliary Relay</th>\
 						<tr><td class="label">Mode:</td><td>' + device.relay_mode + '</td></tr>\
 						<tr><td class="label">Status:</td><td>' + device.relay_status + '</td></tr>\
