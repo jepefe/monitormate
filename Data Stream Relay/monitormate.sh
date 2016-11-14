81f2ce9ad01dd47de27979c11ab77f7a22ed86dd
Timothy Martin <instanttim@me.com>
2016-11-14 13:52:41 -0800

    change arguments to be placeholder
    Instead of being the actual values i use…

--- Data Stream Relay/monitormate.sh
+++ Data Stream Relay/monitormate.sh
@@ -14,7 +14,7 @@
 DIR=/usr/local/bin/monitormate
 DAEMON=$DIR/monitormate.php
 DAEMON_NAME=monitormate
-DAEMON_ARG="-a 10.0.1.4 -p 57027 -u http://finleyridge.com/power/post_datastream.php -t HprJy2oB9rD99Jhq -d"
+DAEMON_ARG="-a 10.0.1.10 -p 57027 -u http://yourwebsite.com/monitormate/post_datastream.php -t y0urT0k3nG03sH3r3!"
 
 # This next line determines what user the script runs as.
 # Root generally not recommended but necessary if you are using the Raspberry Pi GPIO from Python.
